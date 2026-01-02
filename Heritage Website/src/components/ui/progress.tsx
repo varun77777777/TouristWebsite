c8f4e10cef9e6fe14080775478f8adf17430967f
Varun <varun.achar7777@gmail.com>
2026-01-02 18:49:42 +0530

    Update progress.tsx

--- Heritage Website/src/components/ui/progress.tsx
+++ Heritage Website/src/components/ui/progress.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-type Props = { value?: number; max?: number };
+import { cn } from "@/lib/utils"
 
-export const Progress: React.FC<Props> = ({ value = 0, max = 100 }) => (
-  <progress value={value} max={max} />
-);
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20 ",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full rounded-full flex-1 bg-primary transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+))
+Progress.displayName = ProgressPrimitive.Root.displayName
 
-export default Progress;
+export { Progress }
