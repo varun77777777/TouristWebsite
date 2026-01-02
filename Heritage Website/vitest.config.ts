b5836db5b42299af424c8041d187fbaada73ffd7
Varun <varun.achar7777@gmail.com>
2026-01-02 19:18:54 +0530

    Update vitest.config.ts

--- Heritage Website/vitest.config.ts
+++ Heritage Website/vitest.config.ts
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  test: { globals: true, environment: 'jsdom' }
-});
+  // @ts-expect-error - vite-tsconfig-paths is not typed correctly
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
+  esbuild: {
+    target: 'node20',
+  },
+})
