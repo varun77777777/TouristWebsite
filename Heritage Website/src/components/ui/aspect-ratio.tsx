449df900a6cb8a2180cc565b78330a7ef9348fbe
Varun <varun.achar7777@gmail.com>
2026-01-02 11:22:30 +0530

    Update aspect-ratio.tsx

--- Heritage Website/src/components/ui/aspect-ratio.tsx
+++ Heritage Website/src/components/ui/aspect-ratio.tsx
@@ -1,14 +1,5 @@
-import React from 'react';
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
 
-type Props = React.PropsWithChildren<{ ratio?: number }>;
+const AspectRatio = AspectRatioPrimitive.Root
 
-export const AspectRatio: React.FC<Props> = ({ children, ratio = 16 / 9 }) => {
-  const padding = `${100 / ratio}%`;
-  return (
-    <div style={{ position: 'relative', width: '100%', paddingTop: padding }}>
-      <div style={{ position: 'absolute', inset: 0 }}>{children}</div>
-    </div>
-  );
-};
-
-export default AspectRatio;
+export { AspectRatio }
